f10ac67a9ce28f354dd7c27750bb64e6e1f63c9d
anniehuynh <anniehuynh253@gmail.com>
2021-12-02 16:21:42 +0200

    add nav and footer, nav color needs to be checked again mb?

--- login-signup/forms.js
+++ login-signup/forms.js
@@ -1,30 +1,10 @@
-'use strict';
-const url = 'http://localhost:3000'; 
+
 
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
-// select existing html elements
-const addUserForm = document.querySelector('#addUserForm');
 
-// submit registration form
-addUserForm.addEventListener('submit', async (evt) => {
-	evt.preventDefault();
-	const data = serializeJson(addUserForm);
-	const fetchOptions = {
-	  method: 'POST',
-	  headers: {
-		'Content-Type': 'application/json',
-	  },
-	  body: JSON.stringify(data),
-	};
-  
-	const response = await fetch(url + '/user', fetchOptions);
-	const json = await response.json();
-	alert(json.message);
-	location.href = 'forms.html';
-  });
 
 /* log in and sign up btn event listener*/ 
 signUpButton.addEventListener('click', () => {
