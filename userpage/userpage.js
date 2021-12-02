786fd2984cbf2cfbdb18118df9b4ce3e6e4f606e
tamaskr <kralusztomi@gmail.com>
2021-12-02 20:08:37 +0200

    added follow/unfollow switch for ui

--- userpage/userpage.js
+++ userpage/userpage.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const followBtn = document.querySelector('.follow-btn');
+const followBtnText = document.querySelector('.follow-btn a');
+
+followBtn.addEventListener('click', () => {
+    if(followBtn.classList.contains('unfollow')){
+        followBtn.classList.remove('unfollow');
+        followBtnText.textContent = "Follow";
+        return;
+    }
+    followBtn.classList.add('unfollow');
+    followBtnText.textContent = "Unfollow";
+});
