a846955861a2f634351d0d8d8cb50874cdc1e026
anniehuynh <anniehuynh253@gmail.com>
2021-12-04 23:33:49 +0200

    dynamic posts fetch

--- explore-page/explore.js
+++ explore-page/explore.js
@@ -1,15 +1,13 @@
 "use strict";
+const url = "http://localhost:3000";
 
+const createPostCard = (posts) => {
+  const blogs = document.querySelector(".blogs");
+  blogs.setAttribute("class", "blogs");
 
-const blogs = document.querySelector(".blogs");
-blogs.setAttribute("class", "blogs");
-
-const createPostCard =  (posts) => {
-
-  //for(let i = 0; i < 9; i++) {
   posts.forEach((post) => {
     //generate a number and provide to the image to generate randomly
-     //let number = Math.floor(Math.random() * posts.length);
+    //let number = Math.floor(Math.random() * posts.length);
     const a = document.createElement("a");
     const img = document.createElement("img");
     img.setAttribute("height", 450);
@@ -20,32 +18,29 @@ const createPostCard =  (posts) => {
     column.setAttribute("class", "column");
     const h3 = document.createElement("h3");
     h3.setAttribute("id", "title");
-    
 
     img.src = post.image;
     h3.innerHTML = post.title;
 
     console.log(`set img src: ${img.src}`);
+
     a.appendChild(img);
     card.appendChild(h3);
     card.appendChild(a);
-    
     column.appendChild(card);
     blogs.appendChild(column);
-    
   });
 };
 
+const getAllPosts = async () => {
+  try {
+    const response = await fetch(url + "/posts");
+    const posts = await response.json();
+    console.log(posts);
+    createPostCard(posts);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+getAllPosts();
 
-
-const getPosts = async () => {
-  const response = await fetch('http://localhost:3000/posts');
-  const posts = await response.json();
-  console.log(posts);
-  
-   
-  createPostCard(posts);
-  
-}
-
-getPosts();
