2ef1bdf03bc1c1982876bb4de0b75a2c53d4780f
anniehuynh <anniehuynh253@gmail.com>
2021-12-08 18:37:31 +0200

    fetch random post to feed

---
+++ news-feed/feed.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const url = "http://localhost:3000"; // change url when uploading to server
+
+//Get query parameter
+const getQParam = (param) => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  console.log(urlParams.get(param));
+  return urlParams.get(param);
+};
+
+const createPostCard = (posts) => {
+  const blogs = document.querySelector(".post");
+  blogs.setAttribute("class", "card");
+
+  posts.forEach((post) => {
+    const title = document.createElement("h2");
+    title.setAttribute("id", "title");
+    const author = document.createElement("h5");
+    const a = document.createElement("a");
+    const postImg = document.createElement("div");
+    postImg.setAttribute("class", "postImg");
+    const img = document.createElement("img");
+    img.setAttribute("height", 450);
+    img.setAttribute("width", 400);
+    const postDescription = document.createElement("p");
+
+    
+    title.innerHTML = post.title;
+    author.innerHTML = post.author;
+    img.src = post.image;
+    postDescription.innerHTML = post.description;
+
+    console.log(`set img src: ${img.src}`);
+    
+    postImg.appendChild(img);
+    a.appendChild(postImg);
+    blogs.appendChild(title);
+    blogs.appendChild(author);
+    blogs.appendChild(a);
+    blogs.appendChild(postDescription);
+  });
+};
+
+// AJAX calls
+const getPosts = async () => {
+  try {
+    const response = await fetch(url + "/post?limit=4");
+    const posts = await response.json();
+    console.log("fetch random post");
+    createPostCard(posts);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+getPosts(getQParam());
