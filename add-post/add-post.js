643c5d802e43a1aa2e75f891ead33d01cdb5c9d5
anniehuynh <anniehuynh253@gmail.com>
2021-12-08 15:38:23 +0200

    check validation  and log in status

--- add-post/add-post.js
+++ add-post/add-post.js
@@ -11,14 +11,19 @@ addForm.addEventListener('submit', async (evt) => {
   const fd = new FormData(addForm);
   const fetchOptions = {
     method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+    },
     body: fd,
   };
+  
   console.log(fetchOptions);
   const response = await fetch(url + '/post', fetchOptions);
   const json = await response.json();
   alert(json.message);
   location.href = 'add-post.html';
 });
+
 const validationAddPost = () => {
 
   document.write("Post has been created successfully");
