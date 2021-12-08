205ae40f3d5a2fce4976a971a0ecdf304b7412e0
anniehuynh <anniehuynh253@gmail.com>
2021-12-08 21:30:42 +0200

    login.js and serialize added

---
+++ login-signup/login.js
@@ -0,0 +1,75 @@
+'use strict';
+const url = 'http://localhost:3000'; // change url when uploading to server
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+/* log in and sign up btn event listener*/
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
+
+// select existing html elements
+const loginForm = document.querySelector('#logIn');
+const addUserForm = document.querySelector('#signUp');
+
+// login
+loginForm.addEventListener('submit', async (evt) => {
+  evt.preventDefault();
+  const data = serializeJson(loginForm);
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+
+  const response = await fetch(url + '/auth/login', fetchOptions);
+  const json = await response.json();
+  console.log('login response', json);
+  if (!json.user) {
+    alert(json.message);
+  } else {
+    // save token
+    sessionStorage.setItem('token', json.token);
+    sessionStorage.setItem('user', JSON.stringify(json.user));
+    location.href = 'forms.html';
+  }
+});
+
+// submit register form
+addUserForm.addEventListener('submit', async (evt) => {
+  evt.preventDefault();
+  const data = serializeJson(addUserForm);
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(url + '/auth/register', fetchOptions);
+  const json = await response.json();
+  alert(json.message);
+});
+// const validationSignUpForm = () => {
+//     const pw1 = document.getElementById("pw1").value;
+//     const pw2 = document.getElementById("pw2").value;
+//     //check empty confirm password field
+//     if (pw2 == "") {
+//       document.getElementById("message2").innerHTML =
+//         "**Enter the password please!";
+//       return false;
+//     }
+//     if (pw1 != pw2) {
+//       document.getElementById("message2").innerHTML = "Passwords do not match";
+//       return false;
+//     } else {
+//       document.write("SignUp form has been submitted successfully");
+//     }
+//   };
