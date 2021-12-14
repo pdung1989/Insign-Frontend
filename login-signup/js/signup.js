dbd6e2af215a651f5c829049ebb364366c96bf0c
anniehuynh <anniehuynh253@gmail.com>
2021-12-14 14:50:26 +0200

    create roleList selection

--- login-signup/js/signup.js
+++ login-signup/js/signup.js
@@ -3,22 +3,44 @@
 // select existing html elements
 const signUpForm = document.querySelector("#signUpForm");
 const submitRegistration = document.getElementById("submitRegistration");
+const roleList = document.querySelector(".add-role");
 
+//get user role for selection
+const createRoleSelection = (roles) => {
+  let roleArray = [];
+  for (let i = 1; i< roles.length; i++) {
+    roleList.innerHTML += `<option value="${roles[i].role_id}">${roles[i].role_name}</option>`;
+    roleArray.push(roles[i]);
+  }
+};
+//Get role to role options
+const getRoles = async () => {
+  try {
+    const fetchOptions = {
+      method: "GET",
+    };
+    const response = await fetch(url + "/role", fetchOptions);
+    const roles = await response.json();
+    createRoleSelection(roles);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+getRoles();
 //validation form
 submitRegistration.addEventListener("click", () => {
   const pw1 = document.getElementById("pw1").value;
   const pw2 = document.getElementById("pw2").value;
   //check empty confirm password field
-  if (pw1 == "" || pw2 == "") {
-    document.getElementById("message2").innerHTML =
-      "**Enter the password please!";
-    return false;
-  }
+  // if (pw1 == "" || pw2 == "") {
+  //   document.getElementById("message2").innerHTML =
+  //     "**Please enter password!";
+  //   return false;
+  // }
   if (pw1 != pw2) {
     document.getElementById("message2").innerHTML = "Passwords do not match";
     return false;
   }
-
 });
 // submit register form
 signUpForm.addEventListener("submit", async (evt) => {
